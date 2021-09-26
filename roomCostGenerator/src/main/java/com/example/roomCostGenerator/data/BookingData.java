5389ab26a48100760a0e288ae3eab929102492aa
HarshSinghania123 <harshsinghania080697@gmail.com>
2021-09-26 21:45:47 +0530

    Addition of child limit

--- roomCostGenerator/src/main/java/com/example/roomCostGenerator/data/BookingData.java
+++ roomCostGenerator/src/main/java/com/example/roomCostGenerator/data/BookingData.java
@@ -1,6 +1,7 @@
 package com.example.roomCostGenerator.data;
 
 import java.util.Date;
+import java.util.Optional;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
@@ -10,34 +11,34 @@ public class BookingData {
 
 	@Id
 	private String roomId;
-	private Date checkIn;
-	private Date checkOut;
+	private String checkIn;
+	private String checkOut;
 	private int adultCount;
-	private String childAge;
+//	private String childAge;
+	private int child1Age = 0;
+	private int child2Age = 0;
+	private int child3Age = 0;
+	private int child4Age = 0;
+	private int child5Age = 0;
 	private int childCount;
-	
-	public int getChildCount() {
-		return childCount;
-	}
-	public void setChildCount(int childCount) {
-		this.childCount = childCount;
-	}
+	private Date checkInDate;
+	private Date checkoutDate;
 	public String getRoomId() {
 		return roomId;
 	}
 	public void setRoomId(String roomId) {
 		this.roomId = roomId;
 	}
-	public Date getCheckIn() {
+	public String getCheckIn() {
 		return checkIn;
 	}
-	public void setCheckIn(Date checkIn) {
+	public void setCheckIn(String checkIn) {
 		this.checkIn = checkIn;
 	}
-	public Date getCheckOut() {
+	public String getCheckOut() {
 		return checkOut;
 	}
-	public void setCheckOut(Date checkOut) {
+	public void setCheckOut(String checkOut) {
 		this.checkOut = checkOut;
 	}
 	public int getAdultCount() {
@@ -46,17 +47,61 @@ public class BookingData {
 	public void setAdultCount(int adultCount) {
 		this.adultCount = adultCount;
 	}
-	public String getChildAge() {
-		return childAge;
+	public int getChild1Age() {
+		return child1Age;
 	}
-	public void setChildAge(String childAge) {
-		this.childAge = childAge;
+	public void setChild1Age(int child1Age) {
+		this.child1Age = child1Age;
+	}
+	public int getChild2Age() {
+		return child2Age;
+	}
+	public void setChild2Age(int child2Age) {
+		this.child2Age = child2Age;
+	}
+	public int getChild3Age() {
+		return child3Age;
+	}
+	public void setChild3Age(int child3Age) {
+		this.child3Age = child3Age;
+	}
+	public int getChild4Age() {
+		return child4Age;
+	}
+	public void setChild4Age(int child4Age) {
+		this.child4Age = child4Age;
+	}
+	public int getChild5Age() {
+		return child5Age;
+	}
+	public void setChild5Age(int child5Age) {
+		this.child5Age = child5Age;
+	}
+	public int getChildCount() {
+		return childCount;
+	}
+	public void setChildCount(int childCount) {
+		this.childCount = childCount;
+	}
+	public Date getCheckInDate() {
+		return checkInDate;
+	}
+	public void setCheckInDate(Date checkInDate) {
+		this.checkInDate = checkInDate;
+	}
+	public Date getCheckoutDate() {
+		return checkoutDate;
+	}
+	public void setCheckoutDate(Date checkoutDate) {
+		this.checkoutDate = checkoutDate;
 	}
 	
 	@Override
 	public String toString() {
-		return "BookingData [roomId=" + roomId + ", checkIn=" + checkIn + ", checkOut=" + checkOut + ", adultCount="
-				+ adultCount + ", childAge=" + childAge + ", childCount=" + childCount + "]";
+		return "BookingData [roomId=" + roomId + ", adultCount="
+				+ adultCount + ", child1Age=" + child1Age + ", child2Age=" + child2Age + ", child3Age=" + child3Age
+				+ ", child4Age=" + child4Age + ", child5Age=" + child5Age + ", childCount=" + childCount
+				+ ", checkInDate=" + checkInDate + ", checkoutDate=" + checkoutDate + "]";
 	}
 	
 }
